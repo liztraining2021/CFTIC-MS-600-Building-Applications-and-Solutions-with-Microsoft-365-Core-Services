5e766226c08b99f737e31fcbcde8576e6bc804d5
Lizbeth Gómes <totalserviceslgm@gmail.com>
2021-10-01 12:12:30 +0200

    codigo de lab 05 ejercicio 1

--- Lab05/Exer01/My Office Add-in/src/taskpane/taskpane.js
+++ Lab05/Exer01/My Office Add-in/src/taskpane/taskpane.js
@@ -12,30 +12,117 @@ import "../../assets/icon-80.png";
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
+    // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
+    if (!Office.context.requirements.isSetSupported("ExcelApi", "1.7")) {
+      console.log("Sorry. The tutorial add-in uses Excel.js APIs that are not available in your version of Office.");
+    }
+
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    // document.getElementById("run").onclick = run;
+    document.getElementById("create-table").onclick = createTable;
+    document.getElementById("filter-table").onclick = filterTable;
+    document.getElementById("sort-table").onclick = sortTable;
+    document.getElementById("create-chart").onclick = createChart;
+
   }
 });
 
-export async function run() {
-  try {
-    await Excel.run(async (context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
+function createTable() {
+      Excel.run(function (context) {
+        // TODO1: Queue table creation logic here.
+        var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+        var expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
+        expensesTable.name = "ExpensesTable";
+        // TODO2: Queue commands to populate the table with data.
+        expensesTable.getHeaderRowRange().values = [["Date", "Merchant", "Category", "Amount"]];
+        expensesTable.rows.add(null /*add at the end*/, [
+          ["1/1/2017", "The Phone Company", "Communications", "120"],
+          ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
+          ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
+          ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
+          ["1/11/2017", "Bellows College", "Education", "350.1"],
+          ["1/15/2017", "Trey Research", "Other", "135"],
+          ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]
+        ]);
+        // TODO3: Queue commands to format the table.
+        expensesTable.columns.getItemAt(3).getRange().numberFormat = [['€#,##0.00']];
+        expensesTable.getRange().format.autofitColumns();
+        expensesTable.getRange().format.autofitRows();    
+        return context.sync();
+      })
+        .catch(function (error) {
+            console.log("Error: " + error);
+            if (error instanceof OfficeExtension.Error) {
+                console.log("Debug info: " + JSON.stringify(error.debugInfo));
+            }
+        });
+      }
 
-      // Read the range address
-      range.load("address");
+    function filterTable() {
+      Excel.run(function (context) {
+          // TODO1: Queue commands to filter out all expense categories except
+          var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+          var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+          var categoryFilter = expensesTable.columns.getItem('Category').filter;
+          categoryFilter.applyValuesFilter(['Education', 'Groceries']);
+          //        Groceries and Education.
+          return context.sync();
+      })
+      .catch(function (error) {
+          console.log("Error: " + error);
+          if (error instanceof OfficeExtension.Error) {
+              console.log("Debug info: " + JSON.stringify(error.debugInfo));
+          }
+      });
+    }
 
-      // Update the fill color
-      range.format.fill.color = "yellow";
+    function sortTable() {
+      Excel.run(function (context) {
+          // TODO1: Queue commands to sort the table by Merchant name.
+          var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+          var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+          var sortFields = [
+              {
+                  key: 1,            // Merchant column
+                  ascending: false,
+              }
+          ];
+          expensesTable.sort.apply(sortFields);
 
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
+                return context.sync();
+            })
+            .catch(function (error) {
+                console.log("Error: " + error);
+                if (error instanceof OfficeExtension.Error) {
+                    console.log("Debug info: " + JSON.stringify(error.debugInfo));
+                }
+            });
+    }
+
+    function createChart() {
+      Excel.run(function (context) {
+          // TODO1: Queue commands to get the range of data to be charted.
+          var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+          var expensesTable = currentWorksheet.tables.getItem('ExpensesTable');
+          var dataRange = expensesTable.getDataBodyRange();
+          var sortFields = [
+              {
+                  key: 1,            // Merchant column
+                  ascending: false,
+              }
+          ];
+          expensesTable.sort.apply(sortFields);
+
+          // TODO2: Queue command to create the chart and define its type.
+          // TODO3: Queue commands to position and format the chart.
+          return context.sync();
+      })
+      .catch(function (error) {
+          console.log("Error: " + error);
+          if (error instanceof OfficeExtension.Error) {
+              console.log("Debug info: " + JSON.stringify(error.debugInfo));
+          }
+      });
+    } 
+  
